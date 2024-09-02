a1957689c679d9f50415ccb68931c028a0c2e5f8
Damien LOBATO <damien.lobato@Digital-Grenoble.local>
2024-09-02 11:47:41 +0200

    exo 1

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,10 @@
 // you can write js here
-console.log('hello from file');
+let kelvins = prompt("Quelle est la température aujourd'hui en Kelvins ?");
+let celsius = kelvins - 273; //je soustrait la variable kelvins de 273 pour convertir en celsius
+let fahrenheits = celsius * (9/5) + 32 //J'utilise l'opération de conversion pour obtenir le résultat en fahrenheits
+fahrenheits = Math.floor(fahrenheits); //obtenir un entier
+
+
+
+console.log("Celsius:" + celsius);
+console.log("Fahrenheit:" + fahrenheits);
