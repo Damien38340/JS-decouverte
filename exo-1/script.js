379a34d954eb48453e123b16f869c5f239e0c449
Damien LOBATO <damien.lobato@Digital-Grenoble.local>
2024-09-02 15:27:00 +0200

    exo 2

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,6 +1,6 @@
 // you can write js here
 let kelvins = prompt("Quelle est la température aujourd'hui en Kelvins ?");
-let celsius = kelvins - 273; //je soustrait la variable kelvins de 273 pour convertir en celsius
+const celsius = kelvins - 273; //je soustrait la variable kelvins de 273 pour convertir en celsius
 let fahrenheits = celsius * (9/5) + 32 //J'utilise l'opération de conversion pour obtenir le résultat en fahrenheits
 fahrenheits = Math.floor(fahrenheits); //obtenir un entier
 
--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,32 @@
-// you can write js here
-console.log('exo-2');
+let isTesting = true;
+
+const myDate = new Date();
+
+let weekDays = "Time to work!";
+let weekEnd = "Let's chill out a bit";
+
+let today;
+let currentHour;
+
+if (isTesting) {
+    today = prompt("Choose a day");
+    currentHour = prompt("Choose an hour");
+} else {
+    today = myDate.getDay();
+    currentHour = myDate.getHours();
+}
+
+
+if (today === 0 || today === 6) {
+    console.log(weekEnd);
+} else if (today === 5 && currentHour >= 17) { //if we're friday after 5pm
+    console.log(weekEnd);
+} else if (today === 1 && currentHour <= 9) { //if we're monday before 9 am
+    console.log(weekEnd);
+} else {
+    console.log(weekDays);
+}
+
+
+console.log(today);
+console.log(currentHour);
