e509688b9c5d83126f69f4a21f71a81d3b44e78f
Damien LOBATO <damien.lobato@Digital-Grenoble.local>
2024-09-03 09:49:06 +0200

    exo 5

--- exo-4/script.js
+++ exo-4/script.js
@@ -2,4 +2,14 @@
 
 console.log('exo-4');
 
-var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+let secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+secretMessage.pop();
+secretMessage.push("to", "program");
+secretMessage["easily"] = "right";
+secretMessage.shift();
+secretMessage.unshift("Programming");
+secretMessage.splice(5,10-5, "know");
+secretMessage.join();
+
+
+console.log(secretMessage.join(" "));
--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,20 @@
-// you can write js here
+let input = "The owls are not what they seem";
+let vowels = ["A", "E", "I", "O", "U"];
+let resultArray = [];
 
-console.log('exo-5');
+for (let i = 0; i < input.length; i++) {
+    if (vowels.indexOf(input[i].toUpperCase()) !== -1) {
+        resultArray.push(input[i]);
+        console.log(i);
+    }
+}
+
+console.log(resultArray.join(" ").toUpperCase());
+
+
+// for (let i = 0; i < input.length; i++) {
+//     if (vowels.includes(input[i].toUpperCase())){
+//         resultArray.push(input[i]);
+//         console.log(i);
+//     }
+// }
